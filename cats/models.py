cf7901fad6a0f8d833bbef891b7ad174d2475df0
FilipOstarek <githubF@seznam.cz>
2021-05-12 23:14:25 +0200

    visual

--- cats/models.py
+++ cats/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 # Create your models here.
 from django.db import models
+from django.core.validators import MinValueValidator
 
 def fotky(instance, filename):
     return "cats/" + str(instance.id) + "/fotka/" + filename
 
 class Species(models.Model):
-    #Druhy
     jmeno_druhu = models.CharField(max_length=50, unique=True, verbose_name="Druh kočky",
                             help_text='Zadejte prosím druh kočky, popřípadě OwO pokud není známo')
     class Meta:
@@ -15,41 +15,50 @@ class Species(models.Model):
     def __str__(self):
         return self.jmeno_druhu
 
+class Photo(models.Model):
+    fotka = models.ImageField(upload_to='cats/fotky', blank=True, null=True, verbose_name="Fotka")
+    nazev_fotky = models.CharField(max_length=50, blank=True, null=True, verbose_name="Název fotky")
+    popisek_fotky = models.CharField(max_length=500, blank=True, null=True, verbose_name="popisek fotky")
+
+    #druh = models.ForeignKey(Description_of_the_cat, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ["fotka"]
+
+    def __str__(self):
+        return f"{self.fotka}"
+
+
+    @property
+    def filesize(self):
+        x = self.fotka.size
+        y = 512000
+        if x < y * 1000:
+            value = round(x/1024, 2)
+            ext = ' KB'
+        elif x < y * 1000**2:
+            value = round(x/1024*2, 2)
+            ext = ' MB'
+        else:
+            value = round(x/1024**3, 2)
+            ext = ' GB'
+
+        return str(value)+ext
+
 class Description_of_the_cat(models.Model):
-    #popisek kočky
-    '''
-    jmeno = Column(String(50), nullable=True)
-    vek = Column(Integer)
-    vaha = Column(Integer) #kg
-    samotny_popis = Column(Text)
-    '''
     jmeno = models.CharField(max_length=50, verbose_name="Jméno", blank=True)
-    vek = models.IntegerField(verbose_name="Roky Kočky", blank=True)
-    vaha = models.IntegerField(verbose_name="Váha", blank=True)
+    vek = models.IntegerField(verbose_name="Roky Kočky", blank=True, validators=[MinValueValidator(0.0)])
+    vaha = models.IntegerField(verbose_name="Váha", blank=True, validators=[MinValueValidator(0.0)])
     popisek = models.CharField(max_length=500, verbose_name="popisek", blank=True)
+    druh = models.ForeignKey(Species, on_delete=models.CASCADE)
+    fotka = models.ForeignKey(Photo, on_delete=models.CASCADE)
 
-    species = models.ManyToManyField(Species, help_text='Vyberte druh kočky')
-
+    #fotka = models.ImageField(upload_to='cats/fotky/%Y/%m/%d/', blank=True, null=True, verbose_name="Fotka")
+    #species = models.ManyToManyField(Species, help_text='Vyberte fotku kočky')
+    #fotky = models.ManyToManyField(Photo, help_text='Vyberte fotku kočky')
     class Meta:
         ordering = ["jmeno"]
     def __str__(self):
         return f"{self.jmeno}"
 
-class Photo(models.Model):
-    #Fotka
-    nazev_fotky = models.CharField(max_length=50, blank=True, null=True, verbose_name="Název fotky")
-    popisek_fotky = models.CharField(max_length=500, blank=True, null=True, verbose_name="popisek fotky")
-    fotka = models.ImageField(upload_to=fotky, blank=True, null=True, verbose_name="Fotka")
-    '''
-    id = Column(Integer, primary_key=True)
-    url = Column(String(150), unique=True, nullable=False)
-    nazev_fotky = Column(String(length=100))
-    '''
-    class Meta:
-        ordering = ["nazev_fotky"]
-    def __str__(self):
-        """ Textová reprezentace objektu """
-        '''
-        return f"{self.title}, ({self.type})"
-        '''
-        return f"{self.nazev_fotky}"
+
--- cats/views.py
+++ cats/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView, DetailView
+from cats.models import Description_of_the_cat, Species, Photo
+
+def indexu(request):
+    number = Description_of_the_cat.objects.all().count()
+    print(request)
+    context = {
+        'number': number,
+    }
+    return render(request, 'index.html', context=context)
+
+class FilmListView(ListView):
+    print(ListView)
+    model = Description_of_the_cat
+    context_object_name = 'cats_photo'
+    template_name = 'index.html'
+    paginate_by = 12
+
+    def get_queryset(self):
+        return Description_of_the_cat.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['number'] = len(self.get_queryset())
+        context['nazev'] = 'Karty koček'
+        context['nazev_poctu'] = 'Počet záznamů :'
+        return context
